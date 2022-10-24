96e7e00143d319eb39dab3864ebc9c04a8683dda
Santiago Forero <s.forerog2@uniandes.edu.co>
2022-10-24 00:23:42 -0500

    Creación scripts

---
+++ Taller Typescript/scripts/data.js
@@ -0,0 +1,8 @@
+export var series = [
+    new serie(1, "Breaking Bad", "AMC", 5),
+    new serie(2, "Orange Is the New Black", "Netflix", 6),
+    new serie(3, "Game of Thrones", "HBO", 7),
+    new serie(4, "The Big Bang Theory", "CBS", 12),
+    new serie(5, "Sherlock", "BBC", 4),
+    new serie(6, "A Very English Scandal", "BBC", 2),
+];
